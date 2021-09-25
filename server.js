ce6d7e7f3c96ce98b87af85beb3ddd8b435773ad
Nicholas Davis <nickdavis1018@gmail.com>
2021-09-24 19:00:35 -0700

    added bankAccount variable and cleaned up show and create routes

--- server.js
+++ server.js
@@ -11,19 +11,49 @@ app.use(express.static("public"))
 app.use(methodOverride('_method'))
 // END MIDDLEWARE //
 
-app.get('/budget/', (req, res) => {
-  res.render("index.ejs", {budgetItems: budget});
+// MAIN ROUTE //
+
+app.get('/', (req, res) => {
+  res.send("<html>Re-directing to your budget...<script>window.location.href = 'http://localhost:3000/budget'</script></html>")
+  console.log("Redirecting to http://localhost:3000/budget")
 });
+
+// INDEX ROUTE //
+
+app.get('/budget/', (req, res) => {
+  let bankAccount = 0
+  for(let i=0; i < budget.length; i++){
+  bankAccount = bankAccount + budget[i].amount}
+  res.render("index.ejs", {budgetItems: budget, bankAccount: bankAccount})
+});
+
+// NEW ROUTE //
 
 app.get('/budget/new', (req, res) => {
   res.render("new.ejs", {budgetArray: budget})
 })
 
+// CREATE ROUTE //
+
 app.post("/budget", (req, res) => {
+  if(req.body.name == ""){
+    req.body.name = "Not Entered"
+  }
+  if(req.body.from == ""){
+    req.body.from = "Not Entered"
+  }
+  if(req.body.date == ""){
+    req.body.date = "Not Entered"
+  }
+  if(req.body.amount == ""){
+    req.body.amount = "0"
+  }
+  req.body.amount = Number(req.body.amount)
   budget.push(req.body)
   res.redirect("/budget")
-  console.log(req.body)
 })
+
+// SHOW ROUTE
 
 app.get('/budget/:budgetIndex', (req, res) => {
   res.render("show.ejs", {budgetItem: budget[req.params.budgetIndex]});
